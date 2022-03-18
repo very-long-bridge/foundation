1d7e6e8d1513bc0681e2df9599183500b54e51d3
Vadim <lukinvadim101@gmail.com>
2022-03-18 18:06:16 +0300

    all flex

--- script.js
+++ script.js
@@ -10,3 +10,23 @@ tabButtons.forEach(btn => btn.addEventListener('click', (e)=> {
 }));
 
 
+// modal
+const modal = document.getElementById("modal");
+
+const setModalContent = (e)=> {
+  const {src, alt} = e.srcElement;
+  const modalImg = document.getElementById("modalImg");
+  const captionText = document.getElementById("caption");
+
+  modal.style.display = "block";
+  modalImg.src = src;
+  // captionText.innerHTML = alt;
+};
+const warFaceImages = [...document.querySelectorAll('.warface')];
+warFaceImages.forEach(img => img.addEventListener('click', e=> setModalContent(e)));
+
+const span = document.getElementsByClassName("modal")[0];
+
+span.onclick = ()=> { 
+  modal.style.display = "none";
+};
